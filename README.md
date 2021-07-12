edcb456e73a0cc16c07f3b78f903c350bf0bf2ec
Crivez <cryvez@gmail.com>
2021-07-12 13:42:59 -0300

    add new calc

---
+++ README.md
@@ -0,0 +1,5 @@
+# ElmCalc
+
+Calculadora feita em Elm.
+
+Trocar o Main.elm da pasta src com o calc_records para ter uma outra versão da calculadora feita a partir de records
--- src/Main.elm
+++ src/Main.elm
@@ -5,160 +5,89 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
-
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
 --MODEL
 
-type alias Calculator =
-    { add : Float -> Float -> Float,
-      minus : Float -> Float -> Float,
-      times : Float -> Float -> Float,
-      divide : Float -> Float -> Float
-    }
+type Operation = Add | Minus | Multiply
 
-calculator : Calculator
-calculator =
-    { add = (\x y -> x + y),
-      minus = (\x y -> x - y),
-      times = (\x y -> x * y),
-      divide = (\x y -> x / y)      
-    }
 
-type alias Model = 
-    { display : String,
-      function : Float -> Float -> Float,
-      saveValue : Float,
-      append : Bool 
-    }
+type Model  
+    = Oneoperand Int
+    | TwoOperand Int Operation Int
 
 init : Model
 init = 
-    { display = "",
-      function = (\x y -> y) ,
-      saveValue = 0,
-      append = True     
-    }    
-
-parseFloat: String -> Float
-parseFloat input =
-     Maybe.withDefault 0 (String.toFloat(String.replace "," "." input))
-
-operation : Model -> (Float -> Float -> Float) -> Model
-operation model function =
-    { model
-        | function = function,
-          saveValue = (parseFloat model.display),
-          append = False
-    }
+    Oneoperand 0
 
 --UPDATE
 
 type Msg
-  = None
-  | Add
-  | Times
-  | Minus
-  | Divide
-  | Equal
-  | Decimal
-  | Zero
-  | Number Float
-  | Clear
+    = Number Int
+    | Op Operation
+    | Equal
+    | Clear
+    
 
 update : Msg -> Model -> Model
 update msg model = 
-  case msg of
-    None ->
-      model
+    case msg of
+        Number number -> 
+          updateDisplay number model
+        Clear ->
+            init
+        Op Add ->
+            operator Add model    
+        Op Minus ->
+            operator Minus model
+        Op Multiply ->
+            operator Multiply model
+        Equal ->
+            equal model
+            
+        
+        
 
-    Add ->
-      operation model calculator.add
+updateDisplay : Int -> Model -> Model
+updateDisplay number model =
+    case model of
+       Oneoperand x ->
+         Oneoperand ((x*10) + number)
+       TwoOperand x operation y ->
+         TwoOperand x operation ((y*10) + number) 
 
-    Times ->
-      operation model calculator.times
- 
-    Minus ->
-      operation model calculator.minus
- 
-    Divide ->
-      operation model calculator.divide
- 
-    Decimal ->
-      decimal model
- 
-    Equal ->
-      equal model
-    
-    Zero ->
-      zero model
- 
-    Number number ->
-      updateDisplay model number
-
-    Clear ->
-      init
-
-updateDisplay : Model -> Float -> Model
-updateDisplay model number =
-    if (model.append == True) then
-      { model | display = model.display ++ String.fromFloat(number)}
-    else
-      { model
-          | display = String.fromFloat(number),
-            append = True
-      }
+--muda de estado
+operator : Operation -> Model -> Model
+operator newoperation model =
+    case model of
+       Oneoperand x -> 
+         TwoOperand x newoperation 0
+       TwoOperand x op y ->
+         TwoOperand (applyOperation op x y) newoperation 0   --fica no mesmo estado até apertar igual?
 
 equal : Model -> Model
 equal model =
-    if (model.append == True) then
-        { model
-            | display = calculate model,
-              saveValue = parseFloat(model.display),
-              append = False          
-        }
-    else 
-        { model 
-            | display = calculate model,
-              append = False
-        }
+    case model of
+       Oneoperand x ->
+         Oneoperand x
+       TwoOperand x op y ->
+         Oneoperand (applyOperation op x y)
 
-calculate : Model -> String 
-calculate model = 
-    String.replace "." "," (String.fromFloat(model.function model.saveValue (parseFloat model.display)))
+applyOperation : Operation -> Int -> Int -> Int
+applyOperation op x y =
+   case op of
+     Add ->
+       (x + y)
+     Minus ->
+       (x - y)   
+     Multiply ->
+       (x * y)
 
-zero : Model -> Model
-zero model = 
-    if ( String.isEmpty model.display || model.append == False ) then
-        { model 
-            | display = "0",
-              append = False
-        }
-    else 
-        { model
-            | display = model.display ++ "0"  
-        }
 
-decimal : Model -> Model
-decimal model = 
-    if ( String.isEmpty model.display == False ) && ( model.append == True ) then
-        { model 
-            | display = appendDecimal model.display
-        }
-    else
-        { model 
-            | display = "0,",
-              append = True
-        }
+          
+           
 
-appendDecimal : String -> String
-appendDecimal string = 
-    if String.contains "," string then
-        string
-    else
-        string ++ ","
-    
 
 --VIEW
 
@@ -199,35 +128,62 @@ stylesheet =
 view : Model -> Html Msg
 view model =
     div [class "tudo"]  [ h1 [] [text "Calculadora v1"],
-    div [class "calculator"]
-        [ stylesheet,
-          div [class "row"]
-            [ div [class "col-xs-12"]
-                [div [class "display"]
-                    [div [class "display-text"]
-                        [text (model.display)]
-                    ]
-                , div [ class "buttons" ]
-                  [ calculatorButtonWide Clear "C",
-                    calculatorButton (Number 7) "7",
-                    calculatorButton (Number 8) "8",
-                    calculatorButton (Number 9) "9",
-                    calculatorButton Divide "÷",
-                    calculatorButton (Number 4) "4",
-                    calculatorButton (Number 5) "5",
-                    calculatorButton (Number 6) "6",
-                    calculatorButton Times "X",
-                    calculatorButton (Number 1) "1",
-                    calculatorButton (Number 2) "2",
-                    calculatorButton (Number 3) "3",
-                    calculatorButton Minus "-",
-                    calculatorButton Zero "0",
-                    calculatorButton Decimal ",",
-                    calculatorButton Equal "=",
-                    calculatorButton Add "+"                   
-                  ] 
-                ]
-            ]         
-        ]
+      div [class "calculator"]
+          [ stylesheet,
+            div [class "row"]
+              [ div [class "col-xs-12"]
+                  [div [class "display"]
+                      [div [class "display-text"]
+                          [text (displayState model)]
+                      ]
+                  , div [ class "buttons" ]
+                    [ calculatorButtonWide Clear "C",
+                      calculatorButton (Number 7) "7",
+                      calculatorButton (Number 8) "8",
+                      calculatorButton (Number 9) "9",
+                      calculatorButton Clear "÷",
+                      calculatorButton (Number 4) "4",
+                      calculatorButton (Number 5) "5",
+                      calculatorButton (Number 6) "6",
+                      calculatorButton (Op Multiply) "X",
+                      calculatorButton (Number 1) "1",
+                      calculatorButton (Number 2) "2",
+                      calculatorButton (Number 3) "3",
+                      calculatorButton (Op Minus) "-",
+                      calculatorButton Clear "0",
+                      calculatorButton Clear ",",
+                      calculatorButton Equal "=",
+                      calculatorButton (Op Add) "+"                   
+                    ] 
+                  ]
+              ]         
+          ], 
+      div [class "teste"] [text (modelTotext model)]   
     ]
 
+
+displayState : Model -> String
+displayState model =
+    case model of
+      Oneoperand x -> 
+        String.fromInt(x)
+      TwoOperand x operation y ->
+        String.fromInt(y)
+
+modelTotext : Model -> String
+modelTotext model =
+  case model of 
+      Oneoperand x -> 
+        "Oneoperand: " ++ String.fromInt(x)
+      TwoOperand x operation y ->
+        "TwoOperand: " ++ String.fromInt(x) ++ " " ++ operationTotext(operation) ++ " " ++ String.fromInt(y)  
+
+operationTotext : Operation -> String
+operationTotext op =
+   case op of
+     Add ->
+       "Add"
+     Minus ->
+       "Minus"  
+     Multiply ->
+       "Multiply"
